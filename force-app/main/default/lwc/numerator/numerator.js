d053f4adde0eb87798d7fa7a548b466a3a43c733
Aminah Yussuf <ali.ayoolaa@gmail.com>
2025-08-26 10:25:59 -0600

    complete trailhead - "communication from child to parent"

--- force-app/main/default/lwc/numerator/numerator.js
+++ force-app/main/default/lwc/numerator/numerator.js
@@ -1,6 +1,7 @@
-import { LightningElement } from 'lwc';
+import { LightningElement, api } from 'lwc';
 
 export default class Numerator extends LightningElement {
+    @api
     counter = 0;
 
     handleIncrement() {
